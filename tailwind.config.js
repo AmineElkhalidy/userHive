768f1e989f13b73419d7605230ae1082b9506e4f
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2023-01-10 21:48:06 +0100

    Pushing files to the repo...

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      black: {
+        100: "#cccccc",
+        200: "#999999",
+        300: "#666666",
+        400: "#333333",
+        500: "#000000",
+        600: "#000000",
+        700: "#000000",
+        800: "#000000",
+        900: "#000000",
+      },
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
